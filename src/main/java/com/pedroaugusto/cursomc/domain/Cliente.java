227ed5c3e41364ced3e9c720226fbabacd91373b
Pedro Augusto <peedroaugustto@hotmail.com>
2017-09-28 21:57:54 -0300

    Trocando Controle de Serializacao json por apenas JsonIgnore

--- src/main/java/com/pedroaugusto/cursomc/domain/Cliente.java
+++ src/main/java/com/pedroaugusto/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.pedroaugusto.cursomc.domain.enums.TipoCliente;
 
@@ -33,7 +34,7 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 
 	
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -41,7 +42,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
