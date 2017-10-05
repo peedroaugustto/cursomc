b43caa41f4311db3132eafd6de50d32b0b66ac25
Pedro Augusto <peedroaugustto@hotmail.com>
2017-10-05 20:31:28 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/pedroaugusto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pedroaugusto/cursomc/resources/CategoriaResource.java
@@ -40,10 +40,20 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-		public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 			obj.setId(id);
 			obj = service.update(obj);
 			return ResponseEntity.noContent().build();
-		}
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+
+	
 	
 }
--- src/main/java/com/pedroaugusto/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedroaugusto/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.pedroaugusto.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pedroaugusto.cursomc.domain.Categoria;
 import com.pedroaugusto.cursomc.repositories.CategoriaRepository;
+import com.pedroaugusto.cursomc.services.exceptions.DataIntegrityException;
 import com.pedroaugusto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 			find(obj.getId());
 				return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluie uma categoria que possui produtos");	
+		}
 	}
 	
 }
