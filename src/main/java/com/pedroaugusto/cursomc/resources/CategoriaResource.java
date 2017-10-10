6c357bcc63d9ef94cb179c1284d6009627cd8d0f
Pedro Augusto <peedroaugustto@hotmail.com>
2017-10-10 20:05:22 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/pedroaugusto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pedroaugusto/cursomc/resources/CategoriaResource.java
@@ -3,7 +3,7 @@ package com.pedroaugusto.cursomc.resources;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import com.pedroaugusto.cursomc.domain.Categoria;
 import com.pedroaugusto.cursomc.dto.CategoriaDTO;
 import com.pedroaugusto.cursomc.services.CategoriaService;
@@ -62,10 +62,19 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);	
-		
 	}
-	
-
-	
+		
+		@RequestMapping(value="/page", method=RequestMethod.GET)
+		 public ResponseEntity<Page<CategoriaDTO>> findPage(
+				@RequestParam(value="page", defaultValue="0") Integer page,
+				@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+		 		@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+		 		@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		 	Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		 	Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));  
+		 		return ResponseEntity.ok().body(listDto);
+		 }
+		 	
+		
 	
 }
